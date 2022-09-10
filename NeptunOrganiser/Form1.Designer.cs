5faf3c0333df33dd08f612583d43c64dc79ad50e
BenceHargitai <beni.hargitai@gmail.com>
2022-09-10 12:48:56 +0200

    naptár + design kezdete

--- NeptunOrganiser/Form1.Designer.cs
+++ NeptunOrganiser/Form1.Designer.cs
@@ -28,21 +28,287 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.topPanel = new System.Windows.Forms.Panel();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label5 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel6 = new System.Windows.Forms.Panel();
+			this.label6 = new System.Windows.Forms.Label();
+			this.pictureBox5 = new System.Windows.Forms.PictureBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.label10 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label9 = new System.Windows.Forms.Label();
+			this.topPanel.SuspendLayout();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel6.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+			this.panel3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.panel4.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// topPanel
+			// 
+			this.topPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+			this.topPanel.Controls.Add(this.panel1);
+			this.topPanel.Location = new System.Drawing.Point(-3, 0);
+			this.topPanel.Name = "topPanel";
+			this.topPanel.Size = new System.Drawing.Size(1079, 51);
+			this.topPanel.TabIndex = 2;
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(3, 49);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(200, 541);
+			this.panel1.TabIndex = 3;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.label5);
+			this.panel2.Controls.Add(this.pictureBox1);
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Location = new System.Drawing.Point(0, 52);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(175, 96);
+			this.panel2.TabIndex = 4;
+			this.panel2.Click += new System.EventHandler(this.DocxGeneralas);
+			// 
+			// label5
+			// 
+			this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.label5.Location = new System.Drawing.Point(0, 92);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(175, 2);
+			this.label5.TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(61, 6);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(51, 38);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.DocxGeneralas);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(11, 62);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(152, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Word dokumentum generálása";
+			this.label1.Click += new System.EventHandler(this.DocxGeneralas);
+			// 
+			// panel6
+			// 
+			this.panel6.Controls.Add(this.label6);
+			this.panel6.Controls.Add(this.pictureBox5);
+			this.panel6.Controls.Add(this.label7);
+			this.panel6.Location = new System.Drawing.Point(0, 149);
+			this.panel6.Name = "panel6";
+			this.panel6.Size = new System.Drawing.Size(175, 96);
+			this.panel6.TabIndex = 4;
+			this.panel6.Click += new System.EventHandler(this.CsharpProjekt);
+			// 
+			// label6
+			// 
+			this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.label6.Location = new System.Drawing.Point(0, 92);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(175, 2);
+			this.label6.TabIndex = 0;
+			// 
+			// pictureBox5
+			// 
+			this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+			this.pictureBox5.Location = new System.Drawing.Point(61, 6);
+			this.pictureBox5.Name = "pictureBox5";
+			this.pictureBox5.Size = new System.Drawing.Size(51, 38);
+			this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox5.TabIndex = 1;
+			this.pictureBox5.TabStop = false;
+			this.pictureBox5.Click += new System.EventHandler(this.CsharpProjekt);
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(36, 62);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(111, 13);
+			this.label7.TabIndex = 0;
+			this.label7.Text = "C# projekt generálása";
+			this.label7.Click += new System.EventHandler(this.CsharpProjekt);
+			// 
+			// panel3
+			// 
+			this.panel3.Controls.Add(this.label2);
+			this.panel3.Controls.Add(this.pictureBox2);
+			this.panel3.Controls.Add(this.label3);
+			this.panel3.Location = new System.Drawing.Point(0, 246);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(175, 96);
+			this.panel3.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.label2.Location = new System.Drawing.Point(0, 92);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(175, 2);
+			this.label2.TabIndex = 0;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(61, 6);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(51, 38);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			this.pictureBox2.Click += new System.EventHandler(this.CppProjekt);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(31, 62);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(116, 13);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "C++ projekt generálása";
+			this.label3.Click += new System.EventHandler(this.CppProjekt);
+			// 
+			// label4
+			// 
+			this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.label4.Location = new System.Drawing.Point(174, 51);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(2, 700);
+			this.label4.TabIndex = 5;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(11, 566);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(45, 13);
+			this.label8.TabIndex = 6;
+			this.label8.Text = "Órarend";
+			// 
+			// panel4
+			// 
+			this.panel4.Controls.Add(this.label10);
+			this.panel4.Controls.Add(this.button1);
+			this.panel4.Controls.Add(this.label9);
+			this.panel4.Location = new System.Drawing.Point(177, 51);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(902, 536);
+			this.panel4.TabIndex = 7;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(337, 314);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(179, 13);
+			this.label10.TabIndex = 2;
+			this.label10.Text = "Jelenleg nincs feltöltve az órarended";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(32, 98);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(124, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Órarend feltöltése";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click_1);
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label9.Location = new System.Drawing.Point(23, 19);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(144, 39);
+			this.label9.TabIndex = 0;
+			this.label9.Text = "Órarend";
 			// 
 			// Form1
 			// 
+			this.AllowDrop = true;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(1075, 588);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel6);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.topPanel);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
+			this.topPanel.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel6.ResumeLayout(false);
+			this.panel6.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.panel4.ResumeLayout(false);
+			this.panel4.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+		private System.Windows.Forms.Panel topPanel;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Panel panel6;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.PictureBox pictureBox5;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label9;
 	}
 }
 
--- NeptunOrganiser/Form1.cs
+++ NeptunOrganiser/Form1.cs
@@ -6,19 +6,249 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+using SautinSoft.Document;
+using System.Globalization;
 using System.Windows.Forms;
+
+
+using HorizontalAlignment = SautinSoft.Document.HorizontalAlignment;
+using System.Diagnostics;
 
 namespace NeptunOrganiser
 {
 	public partial class Form1 : Form
 	{
+		class Tanora
+		{
+			public DateTime start { get; set; }
+			public DateTime end { get; set; }
+			public string description { get; set; }
+			public Tanora()
+			{
+				start = new DateTime();
+				end = new DateTime();
+				description = "";
+			}
+		}
+		public static string nevgeneralas()
+		{
+			string ev = DateTime.Now.Year.ToString() + ".";
+			string honap = DateTime.Now.Month.ToString()[0].ToString() != "0" ? "0" + DateTime.Now.Month.ToString()+"." : DateTime.Now.Month.ToString() + ".";
+			string nap =  DateTime.Now.Day.ToString()[0].ToString() != "0" ? "0" + DateTime.Now.Day.ToString() +".": DateTime.Now.Day.ToString() + ".";
+
+			return ev + honap + nap;
+		}
+		public static void CreateDocxUsingDocumentBuilder()
+		{
+			// Set a path to our document.
+			DocumentCore dc = new DocumentCore();
+			string nev = "ProgramozasAlapok20 - " + nevgeneralas();
+			string filePath = @"A:\" + nev + ".docx";
+
+			ParagraphStyle paragraphStyle1 = new ParagraphStyle("ParagraphStyle1");
+			paragraphStyle1.CharacterFormat.FontName = "Avenir Next LT Pro Demi";
+			paragraphStyle1.CharacterFormat.Size = 9;
+			dc.Styles.Add(paragraphStyle1);
+
+			ParagraphStyle paragraphStyle2 = new ParagraphStyle("ParagraphStyle2");
+			paragraphStyle2.CharacterFormat.FontName = "Avenir Next LT Pro Demi";
+			paragraphStyle2.CharacterFormat.Size = 22;
+			dc.Styles.Add(paragraphStyle2);
+
+			ParagraphStyle paragraphStyle3 = new ParagraphStyle("ParagraphStyle3");
+			paragraphStyle3.CharacterFormat.FontName = "Bahnschrift SemiBold";
+			paragraphStyle3.CharacterFormat.Size = 11;
+			dc.Styles.Add(paragraphStyle3);
+
+
+			dc.Sections.Add(
+				new Section(dc,
+					new Paragraph(dc, (DateTime.Now.Year).ToString() + "." + (DateTime.Now.Month).ToString() + "." + (DateTime.Now.Day).ToString())
+					{
+						ParagraphFormat = new ParagraphFormat
+						{
+							Alignment = HorizontalAlignment.Right,
+							Style = paragraphStyle1
+
+						}
+					},
+					new Paragraph(dc, "Programozás alapok 20.")
+					{
+						ParagraphFormat = new ParagraphFormat
+						{
+							LineSpacingRule = LineSpacingRule.Exactly,
+							SpaceBefore = 0,
+							SpaceAfter = 34,
+							Alignment = HorizontalAlignment.Center,
+							Style = paragraphStyle2
+						}
+					},
+					new Paragraph(dc, "")
+					{
+						ParagraphFormat = new ParagraphFormat
+						{
+							LineSpacingRule = LineSpacingRule.Exactly,
+							SpaceBefore = 10,
+							Alignment = HorizontalAlignment.Left,
+							Style = paragraphStyle3
+						}
+					}));
+
+			dc.Save(filePath);
+			System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(filePath) { UseShellExecute = true });
+		}
+		public static void SLNProjektManager()
+		{
+
+			string targetPath = @"A:\";
+			bool useCurrentTime = true;
+			byte mode = 0;
+			using (StreamReader f = new StreamReader("settings.txt"))
+			{
+				string[] temp = f.ReadLine().Split('"');
+				//targetPath = temp[1];
+				Console.WriteLine(temp[1]);
+				temp = f.ReadLine().Split('"');
+				if (temp[1] == "False")
+					useCurrentTime = false;
+
+				temp = f.ReadLine().Split('"');
+				if (temp[1] == "1")
+					mode = 1;
+			}
+
+			if (useCurrentTime)
+				targetPath += @"\" + DateTime.Now.ToString("yyyy.MM.dd") + " - " + DateTime.Now.ToString("HH") + "h" + DateTime.Now.ToString("mm") + "m";
+
+			List<string> sourcePath = new List<string>();
+			sourcePath.Add(Directory.GetCurrentDirectory() + @"\csharp_template");
+			sourcePath.Add(Directory.GetCurrentDirectory() + @"\cplusplus_template");
+
+			Console.WriteLine(Directory.GetCurrentDirectory() + @"\csharp_template");
+
+			foreach (string dirPath in Directory.GetDirectories(sourcePath[mode], "*", SearchOption.AllDirectories))
+			{
+				Directory.CreateDirectory(dirPath.Replace(sourcePath[mode], targetPath));
+			}
+
+			//Copy all the files & Replaces any files with the same name
+			foreach (string newPath in Directory.GetFiles(sourcePath[mode], "*.*", SearchOption.AllDirectories))
+			{
+				File.Copy(newPath, newPath.Replace(sourcePath[mode], targetPath), true);
+			}
+		}
+		public static void Beolvaso()
+		{
+			List<Tanora> tanorak = new List<Tanora>();
+
+			try
+			{
+				using (StreamReader f = new StreamReader("CalendarExport.ics"))
+					while (!f.EndOfStream)
+					{
+						string[] temp = f.ReadLine().Split(':');
+						if (temp[0] == "DTSTART")
+						{
+							temp[1] = temp[1].Remove(8, 1);
+							temp[1] = temp[1].Remove(14, 1);
+
+							tanorak.Add(new Tanora());
+							tanorak[tanorak.Count - 1].start = DateTime.ParseExact(temp[1], "yyyyMMddHHmmss", CultureInfo.InvariantCulture);
+						}
+						else if (temp[0] == "DTEND")
+						{
+							temp[1] = temp[1].Remove(8, 1);
+							temp[1] = temp[1].Remove(14, 1);
+
+							tanorak[tanorak.Count - 1].end = DateTime.ParseExact(temp[1], "yyyyMMddHHmmss", CultureInfo.InvariantCulture);
+						}
+						else if (temp[0] == "SUMMARY")
+							tanorak[tanorak.Count - 1].description = temp[1];
+					}
+			}
+			catch(FileNotFoundException)
+			{
+				MessageBox.Show("Az adott fájl nem található");
+			}
+		}
+
+
 		public Form1()
 		{
 			InitializeComponent();
+			topPanel.BackColor = ColorTranslator.FromHtml("#2995fe");
+			panel2.Cursor = System.Windows.Forms.Cursors.Hand;
+			panel3.Cursor = System.Windows.Forms.Cursors.Hand;
+			panel6.Cursor = System.Windows.Forms.Cursors.Hand;
+
+			if (System.IO.File.Exists("CalendarExport.ics"))
+			{
+				label10.Hide();
+			}
+
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
+			nevgeneralas();
+		}
+
+
+
+		private void DocxGeneralas(object sender, EventArgs e)
+		{
+			CreateDocxUsingDocumentBuilder();
+
+		}
+
+		private void CsharpProjekt(object sender, EventArgs e)
+		{
+			SLNProjektManager();
+		}
+
+
+		private void CppProjekt(object sender, EventArgs e)
+		{
+			SLNProjektManager();
+		}
+
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.CheckFileExists = true;
+			openFileDialog.AddExtension = true;
+			openFileDialog.Multiselect = false;
+			openFileDialog.Filter = "Naptár fileok|*.ics";
+
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				var fileName = openFileDialog.FileName;
+				try
+				{
+					System.IO.File.Copy(fileName, Path.Combine(@".\", "CalendarExport" + ".ics"));
+					MessageBox.Show("Az importálás sikeres volt!");
+					label10.Hide();
+				}
+				catch
+				{
+					DialogResult dialogResult = MessageBox.Show("Már van beimportálva egy órarend. Kívánod felülírni?", "Import", MessageBoxButtons.YesNo);
+					if (dialogResult == DialogResult.Yes)
+					{
+						System.IO.File.Copy(fileName, Path.Combine(@".\", "CalendarExport" + ".ics"), true);
+						MessageBox.Show("Az importálás sikeres volt!");
+
+					}
+					else
+					{
+						MessageBox.Show("Az importálás sikertelen volt!");
+					}
+				}
+			}
+
 
 		}
 	}
